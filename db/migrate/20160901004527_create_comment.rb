86b51f22b4d474df715660375564505e5cd90874
Bryan Goss <bcgoss@gmail.com>
2016-09-01 07:22:26 -0600

    Battry is dyingg

---
+++ db/migrate/20160901004527_create_comment.rb
@@ -0,0 +1,9 @@
+class CreateComment < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :author_name
+      t.text :body
+      t.references :article, index: true, foreign_key: true
+    end
+  end
+end
